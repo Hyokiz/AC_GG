74ba4ab55ad31db3826621baf47707c92162989a
sarazasi <yamadatakumin@icloud.com>
2024-11-30 23:36:23 +0900

    test

---
+++ yamada_works/AtCoder_Beginner_Contest_382/C-Answer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int k = 200010;
+
+int main(){
+    int n, m;
+
+    cin >> n >> m;
+
+    vector<int> a, b;
+    vector<int> id(k, -1);
+
+    for(int i = 0; i<n;){
+        d
+    }
+}
