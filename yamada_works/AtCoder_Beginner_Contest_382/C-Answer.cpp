bd9410f5fab68ce0b042e549024ac9395f1f8755
sarazasi <yamadatakumin@icloud.com>
2024-12-01 00:01:55 +0900

    11/30 ABC382 Cまで

--- yamada_works/AtCoder_Beginner_Contest_382/C-Answer.cpp
+++ yamada_works/AtCoder_Beginner_Contest_382/C-Answer.cpp
@@ -5,14 +5,25 @@ using namespace std;
 const int k = 200010;
 
 int main(){
-    int n, m;
+    /* --- 人の美食度 a おいしさ b ---*/
+    int n, m;// n:人 m:寿司
 
     cin >> n >> m;
-
-    vector<int> a, b;
     vector<int> id(k, -1);
 
-    for(int i = 0; i<n;){
-        d
+    int r = k;
+    for(int i = 0; i<n; i++){
+        int a;
+        cin >> a; //美食度
+
+        while(r > a){
+            --r;
+            id[r] = i + 1;
+        }
+    }
+    for(int i = 0; i<m; i++){
+        int b;
+        cin >> b;
+        cout << id[b] << endl;
     }
 }
